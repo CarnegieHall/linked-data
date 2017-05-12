865b5b21bca55f648b8ffd4667cf67ef6fcf4482
Rob Hudson <robert.matthew.hudson@gmail.com>
2017-05-12 08:31:34 -0400

    Adding script that scrapes data from GeoNames.

---
+++ get_geoInfo.py
@@ -0,0 +1,63 @@
+# !/usr/local/bin/python3.4
+
+## Argument[0] is script to run
+## Argument[1] is path to entityDict
+
+import httplib2
+import json
+import lxml
+import os
+import re
+import sys
+import time
+from bs4 import BeautifulSoup
+from rdflib import Graph, Literal, Namespace, URIRef
+from rdflib.namespace import RDFS
+from rdflib.plugins.serializers.nt import NTSerializer
+
+gPlaces = Graph()
+gn = Namespace('http://www.geonames.org/ontology#')
+wgs84_pos = Namespace('http://www.w3.org/2003/01/geo/wgs84_pos#')
+
+filePath_1 = sys.argv[1]
+
+with open(filePath_1, 'rU') as f1:
+    entities = json.load(f1)
+    for entity in entities:
+        geobirth = entities[entity]['geobirth']
+        if geobirth:
+            uri = ''.join([geobirth, 'about.rdf'])
+            h = httplib2.Http()
+            resp, rdf_doc = h.request(uri, "GET")
+            time.sleep(1.5)
+            soup = BeautifulSoup(rdf_doc, "xml")
+
+            for tag in soup.find_all("name"):
+                name = tag.text
+                gPlaces.add( (URIRef(uri), RDFS.label, Literal(name)) )
+
+            for tag in soup.find_all("parentADM1"):
+                state = tag.attrs['rdf:resource']
+                gPlaces.add( (URIRef(uri), gn.parentADM1, URIRef(state) ) )
+
+            for tag in soup.find_all("parentCountry"):
+                country = tag.attrs['rdf:resource']
+                gPlaces.add( (URIRef(uri), gn.parentCountry, URIRef(country) ) )
+
+            for tag in soup.find_all("lat"):
+                lat = tag.text
+                gPlaces.add( (URIRef(uri), wgs84_pos.lat, Literal(lat)) )
+
+            for tag in soup.find_all("long"):
+                long = tag.text
+                gPlaces.add( (URIRef(uri), wgs84_pos.long, Literal(long)) )
+
+places_graph_path = os.path.join(os.path.dirname(__file__), os.pardir, 'Graphs', 'placesGraph_test.nt')
+
+gPlaces.bind("gn", gn)
+gPlaces.bind("rdfs", RDFS)
+gPlaces.bind("wgs84_pos", wgs84_pos)
+
+gPlaces = gPlaces.serialize(destination=places_graph_path, format='nt')
+
+print('Finished getting geo info')
