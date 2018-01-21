6c6e5e7db8149161b6e708048d9e84ca55997fe1
Rob Hudson <robertmhudson4@gmail.com>
2018-01-21 13:19:47 -0500

    Finalized effective matching

--- scripts/match_mbzInstruments.py
+++ scripts/match_mbzInstruments.py
@@ -18,7 +18,6 @@ filePath_1 = sys.argv[1]
 filePath_2 = sys.argv[2]
 
 ch_toMbzDict = {}
-mbzDict = {}
 
 mbzInstruments = pd.read_csv(filePath_1)
 chInstruments = pd.read_csv(filePath_2)
@@ -38,43 +37,25 @@ FuzzyWuzzyResults = chInstruments.loc[:, "label"].apply(
 
 for result in FuzzyWuzzyResults:
     if result:
-        print(mbzInstruments.iloc[result[2],0])
+        chLabel = result[0]
+        matchID = mbzInstruments.iloc[result[2],0]
+        matchLabel = mbzInstruments.iloc[result[2],1]
+        matches = chInstruments.loc[
+            chInstruments['label'] == str(matchLabel)]
+        df = matches.set_index('instrument')
 
-##with open(filePath_1, 'rU') as f1:
-##    mbzInstruments = csv.reader(f1, dialect='excel', delimiter=',')
-##    for row in mbzInstruments:
-##        mbz_id = row[0]
-##        mbz_label = row[1]
-##
-##        mbzDict[str(mbz_id)] = mbz_label
-##
-##with open(filePath_2, 'rU') as f2:
-##    chData = json.load(f2)
-##
-##    for result in chData["results"]["bindings"]:
-##        chURI = result["instrument"]["value"]
-##        chLabel = result["label"]["value"]
-##
-##        for key in mbzDict:
-##            mbz_label = mbzDict[key]
-####            match_pct = fuzz.token_set_ratio(mbz_label, chLabel)
-##            match_pct = fuzz.partial_ratio(mbz_label, chLabel)
-####            match_pct = fuzz.token_sort_ratio(mbz_label, chLabel)
-##            if match_pct > 75:
-####                print(key, '\t', mbz_label, '\t', chLabel)
-##
-##                ch_toMbzDict[str(chURI)] = {}
-##                ch_toMbzDict[str(chURI)]["label"] = chLabel
-##                ch_toMbzDict[str(chURI)]["matches"] = {}
-##                ch_toMbzDict[str(chURI)]["matches"][str(key)] = mbz_label
-##
-##print(len(ch_toMbzDict))
-##print (json.dumps(ch_toMbzDict, indent=4))
+        if not df.empty:
+            chID = df.index.values[0]
 
-##ch_toMbzDict_path = os.path.join(
-##    os.path.dirname(__file__), os.pardir, 'JSON_dicts', 'ch_toMbzDict.json')
-##
-##with open(ch_toMbzDict_path, 'w') as f1:
-##    json.dump(ch_toMbzDict, f1)
-##
-##print("Finished finding matches to MusicBrainz instrument IDs")
+            ch_toMbzDict[str(chID)] = {}
+            ch_toMbzDict[str(chID)]['label'] = chLabel
+            ch_toMbzDict[str(chID)]['mbz label'] = matchLabel
+            ch_toMbzDict[str(chID)]['mbz ID'] = matchID
+
+ch_toMbzDict_path = os.path.join(
+    os.path.dirname(__file__), os.pardir, 'JSON_dicts', 'ch_toMbzDict.json')
+
+with open(ch_toMbzDict_path, 'w') as f1:
+    json.dump(ch_toMbzDict, f1)
+
+print("Finished finding matches to MusicBrainz instrument IDs")
