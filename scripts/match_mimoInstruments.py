091617b55cc3e2662570bb3297ee2530ddeb70e1
Rob Hudson <robert.matthew.hudson@gmail.com>
2019-02-06 13:06:39 -0500

    Added match count

--- scripts/match_mimoInstruments.py
+++ scripts/match_mimoInstruments.py
@@ -2,6 +2,8 @@
 # ----Copyright (c) 2017 Carnegie Hall | The MIT License (MIT)----
 # ----For the full license terms, please visit https://github.com/CarnegieHall/linked-data/blob/master/LICENSE----
 
+## Matches CH instrument IDs to Wikidata and MIMO IDs using existing
+## MusicBrainz ID alignment
 ## Argument[0] is script to run
 ## Argument[1] is path to csv of Wikidata query results w/MIMO and MBZ IDs
 ## Argument[2] is path to csv file of CH instrument IDs, labels, MBZ links
@@ -11,8 +13,6 @@ import csv
 import json
 import os
 import sys
-##from fuzzywuzzy import fuzz
-##from fuzzywuzzy import process
 from rdflib import Graph, Literal, Namespace, URIRef
 from rdflib.namespace import FOAF, RDF, RDFS, SKOS, XSD
 from rdflib.plugins.serializers.nt import NTSerializer
@@ -26,12 +26,7 @@ ch_toMIMO_Dict = {}
 
 g = Graph()
 
-##def fuzzy_match(x, choices, scorer, cutoff):
-##    return process.extractOne(
-##        x, choices=choices, scorer=scorer, score_cutoff=cutoff
-##        )
-
-with open(filePath_1, 'rU') as f1:
+with open(filePath_1, encoding='utf-8', errors='ignore') as f1:
     wikidataInstr = csv.reader(f1, dialect='excel', delimiter=',', quotechar='"')
     next(wikidataInstr, None)
     for row in wikidataInstr:
@@ -45,7 +40,7 @@ with open(filePath_1, 'rU') as f1:
         wikidataInstr_dict[str(mbzID)]['wikidataID'] = wikidataID
         wikidataInstr_dict[str(mbzID)]['mimo'] = mimoID
 
-with open(filePath_2, 'rU') as f2:
+with open(filePath_2, encoding='utf-8', errors='ignore') as f2:
     chInstruments = csv.reader(f2, dialect='excel', delimiter=',', quotechar='"')
     next(chInstruments, None)
     for row in chInstruments:
@@ -79,5 +74,5 @@ g = g.serialize(destination=ch_toMIMO_Graph_path, format='nt')
 with open(ch_toMIMO_Dict_path, 'w') as f1:
     json.dump(ch_toMIMO_Dict, f1)
 
-print(json.dumps(ch_toMIMO_Dict, indent=4))
 print("Finished finding matches to MIMO instrument IDs")
+print(len(ch_toMIMO_Dict), "instruments matched")
